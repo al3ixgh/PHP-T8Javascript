1df5be575542307dad838104666f8e35010ba034
aleix <al3ixgh@gmail.com>
2021-05-25 19:21:06 +0200

    primer commit

---
+++ js/ej11.js
@@ -0,0 +1,14 @@
+function miFunc() {
+    var d1,d2;
+    var cadena=[];
+    const TIRADAS=36000
+    var suma
+    console.log("d1 =",d1);
+    console.log("d2 =",d2);
+
+    for(i=0;i<TIRADAS;i++){
+        suma=(Math.round(Math.random() * (6 - 1) + 1))+(Math.round(Math.random() * (6 - 1) + 1));
+        cadena.push(suma)
+    }
+    console.log(cadena);
+}
---
+++ js/ej12.js
@@ -0,0 +1,55 @@
+//valida el email pasado como parametro
+function validarEmail(cadena) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(cadena)) {
+        alert("La dirección de email " + cadena + " es correcta.");
+    } else {
+        alert("La dirección de email es incorrecta.");
+    }
+}
+//captura el valor introducido en el input y lo trata llamando a la funcion
+//validarEmail()
+function miFuncEmail() {
+    var email = document.getElementById("email").value
+    console.log(email);
+    //validar email
+    console.log("la cadena es: ", email);
+    validarEmail(email)
+}
+//valida la fecha pasada como parametro
+function validarFecha(fecha) {
+    if (/^\w*\d{2}\/\d{2}\/\d{4}$/.test(fecha)) {
+        alert("la fecha es correcta");
+    }
+    else {
+        alert("la fecha es incorrecta")
+    }
+}
+//captura el valor introducido en el input y lo trata llamando a la funcion
+//validarFecha()
+function miFuncFecha() {
+    var fecha = document.getElementById("fecha").value
+    console.log(fecha);
+    //validar fecha
+    console.log("la cadena es: ", fecha);
+    validarFecha(fecha)
+}
+
+
+//EJERCICIO ACTUAL 
+function escapeHTML(cadena) {
+    if (/([\&\\\<\>]?)$/.test(cadena)) {
+        alert("la cadena contiene algun caracter especial");
+    }
+    else {
+        alert("la cadena no contiene ningun caracter especial");
+    }
+    var cadena2 = cadena.replace(/\&/gi, 'asdg')
+    console.log(cadena2);
+}
+
+function miFuncCadena() {
+    var cadena = document.getElementById("cadena").value;
+    console.log(cadena);
+    escapeHTML(cadena);
+}
+
